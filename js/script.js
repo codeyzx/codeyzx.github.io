6d58d4d411562ffa9b2d8a6f2fd43f02b09f12ca
codeyzx <yahyatruth@gmail.com>
2024-04-04 10:50:31 +0700

    remove unnecessary

--- js/script.js
+++ js/script.js
@@ -4,16 +4,3 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-
-function initMap() {
-  // Specify the coordinates where you want the map to be centered
-  var myLatLng = { lat: -34.397, lng: 150.644 };
-
-  // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById("map"), {
-    center: myLatLng,
-    zoom: 8, // Adjust the zoom level as needed
-  });
-
-  // Optionally, you can add markers or other features to the map here.
-}
